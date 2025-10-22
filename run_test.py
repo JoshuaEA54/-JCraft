1c50ccef53ee45f690ab9f300a4db8f96cf0dd6e
JazminGamboaChacon <154479555+JazminGamboaChacon@users.noreply.github.com>
2025-10-22 12:42:36 -0600

    Add test script for :JCraft language execution with sample source code

---
+++ run_test.py
@@ -0,0 +1,9 @@
+﻿from lang.interpreter import run_source
+src = """
+mesa_crafteo vacío main():
+  bloques vidas = 3;
+  letrero "Vidas: " + vidas;
+fin
+"""
+out = run_source(src, input_callback=lambda p: "entrada_demo", debug=True)
+print("RESULTS:", out)
