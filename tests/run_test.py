dc6d6eee6b2cb723dfdab59a610c403990acb071
JazminGamboaChacon <154479555+JazminGamboaChacon@users.noreply.github.com>
2025-11-14 01:54:19 -0600

    Delete tests directory

--- tests/run_test.py
+++
@@ -1,9 +0,0 @@
-﻿from lang.interpreter import run_source
-src = """
-mesa_crafteo vacío main():
-  bloques vidas = 3;
-  letrero "Vidas: " + vidas;
-fin
-"""
-out = run_source(src, input_callback=lambda p: "entrada_demo", debug=True)
-print("RESULTS:", out)
